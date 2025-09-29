a12f4c14caf3c217d31bf485727552a6c9b90cf0
aravindV14 <167337333+aravindV14@users.noreply.github.com>
2025-09-29 19:45:17 +0530

    Create Calculatormain.java
    commiting the changes after writing the java basic calculator code

---
+++ Calculatormain.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Calculatormain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1, num2, result;
+        char operator;
+
+        System.out.println("==== Basic Calculator ====");
+        System.out.print("Enter first number: ");
+        num1 = sc.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /, %): ");
+        operator = sc.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        num2 = sc.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result = " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result = " + result);
+                } else {
+                    System.out.println("Error! Division by zero is not allowed.");
+                }
+                break;
+
+            case '%':
+                if (num2 != 0) {
+                    result = num1 % num2;
+                    System.out.println("Result = " + result);
+                } else {
+                    System.out.println("Error! Modulus by zero is not allowed.");
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operator!");
+                break;
+        }
+
+        sc.close();
+    }
+}
